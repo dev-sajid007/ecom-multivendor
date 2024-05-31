b0efe63879bb51faeb2da06f0b68380228581df9
dev-sajid007 <dev.sajid007@gmail.com>
2024-05-31 05:45:40 -0400

    Admin Mastering Complete

---
+++ routes/vendor.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\VendorController;
+
+
+
+
+Route::get('/dashboard',[VendorController::class,'dashboard'])->name('dashboard');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
