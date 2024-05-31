9d3bce9174e14f1b9df2c5031e975d10d031dc17
dev-sajid007 <dev.sajid007@gmail.com>
2024-05-31 16:13:07 -0700

    Profile Update Complete

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get('/dashboard',[AdminController::class,'dashboard'])->name('dashboard');
 
-
+//profile routes
+Route::get('/profile',[ProfileController::class,'profile'])->name('profile');
+Route::post('/profile/update',[ProfileController::class,'profileUpdate'])->name('profile.update');
